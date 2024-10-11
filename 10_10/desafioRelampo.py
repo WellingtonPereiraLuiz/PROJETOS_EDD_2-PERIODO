eed934839ebc2266fcbb2f60400eb05d989a7621
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2024-10-10 23:17:54 -0300

    novo

---
+++ 10_10/desafioRelampo.py
@@ -0,0 +1,15 @@
+import numpy as np
+from array import array
+
+"""
+valoresInteiros = array('i', [])
+
+for i in range(-10, 11):
+    valoresInteiros.append(i)
+
+print(valoresInteiros)
+"""
+
+float32_arr = np.array([1.0, 2.0, 3.0], dtype=np.float32)
+
+# for i in range(0, 100):
