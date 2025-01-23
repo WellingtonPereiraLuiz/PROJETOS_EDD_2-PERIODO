e0ad6fce6392005aa1aaa661d3cec1a2a3da2e33
WellingtonPereiraLuiz <wellingtonpereiraluiz89@gmail.com>
2025-01-23 18:21:39 -0300

    arrumando

--- 2025/16_01/16_01.py
+++ 2025/16_01/16_01.py
@@ -30,7 +30,7 @@ print(f'\nO quadrado dos numeros da lista é: {quadrados}')
 numeros = [1, -4, -2, 3, -1, 5, -6]
 positivos = {n for n in numeros if n > 0}
 print(f'\nOs numeros positivos são: {positivos}')
-"""
+
 
 # listas  aninhadas  e  comprehensions;  
 # expressões  lambdas      e  funções Integradas. 
@@ -47,3 +47,66 @@ print(f'ou \n')
 # Lista aninhada: matriz 3 x 3  de nomes
 listas = [[1, 2, 3], ['Josefina', 'Astrogildo', 'Gestronilda'] , ['F', 'M', 'F']]
 agrupadas = [' '.join(map(str, tupla)) for tupla in zip(*listas)]
+print(f'\nOs elementos agrupados são: {"; ".join(agrupadas)} \n')
+
+# Lambda
+#Exemplos:
+
+#Funcao comun
+def funcao_1(x):
+    return x + 1
+print(f'\nO valor retornado é {funcao_1(9)}')
+
+#Funcao lambda com variaveis tipadas
+funcao_1 = lambda x : x + 1
+print(f'\nO valor retornado é:{funcao_1(9)}')
+
+#Expressões lambda com mais de um parâmetro de entrada 
+nome_completo = lambda nome, sobrenome : nome.strip().title() + " " + sobrenome.strip()
+print(f'\nInstituição: {nome_completo("IFRO", "Campus Ariquemes")}')
+print(f'\nCurso: {nome_completo("Análise", "e Desenvolvimento de Sistemas - ADS")}')
+
+# Ordenando listas usando expressão lambda
+atores = ["Leonardo DiCarpio",
+          "Brad Pitt",
+          "Dezel Washington",
+          "philip Seymour Hoffman",
+          "Jonhny Depp"]
+atores.sort(key=lambda sobrenome: sobrenome.split(" ")[-1].lower())
+print(f'\nLista ordenada: {atores}\n')
+
+#Lambda para calcular funções quadráticas
+def funcao_quadratica(a, b, c):
+    return lambda x : a * x ** 2 + b * x + c
+
+quadratica = funcao_quadratica(2, 3, -5)
+
+print(f'\nFuncao quadratica de 0 = {quadratica(3)}')
+print(f'\nFuncao quadratica de 1 = {quadratica(6)}')
+print(f'\nFuncao quadratica de 2 = {quadratica(9)} \n')
+
+#Lambda para evitar escrever duas funções 
+def somar(x):
+    funcao_2 = lambda x : x + 9
+    return funcao_2(x) * 1
+
+print(f'\nO valor retornado é: {somar(4)}')
+
+#Lambda - função sem a palavra reservada def() 
+a = lambda x : x * 2
+print(f'\nO valor da expressãp é: {a(6)}\n')
+"""
+ #Diferentes entradas de dados para funções lambda
+sem_entrada = lambda: 'Estrutura de Dados'
+uma_entrada = lambda x: 5*x+3
+duas_entradas = lambda x, y: (x*y) ** 0.8
+tres_entradas = lambda x, y, z: 5 / ( 2 / x + 2 / y + 2 / z)
+
+print
+
+
+
+
+
+
+
